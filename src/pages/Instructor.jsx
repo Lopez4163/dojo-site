42780a6fc837d5708562cadce38fecf4a75f2f67
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-01-10 17:32:42 -0600

    yerp

--- src/pages/Instructor.jsx
+++ src/pages/Instructor.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import InstructorLoginForm from '../Componenets/InstructorLoginForm'
 const Instructor = () => {
     return (
-        <div>Instructor</div>
+        <div className='student-login-container'>
+            <div className='student-login-content'>
+                <section>
+                    <div className='student-login-form'>
+                        <InstructorLoginForm/>
+                    </div>
+                </section>
+            </div>
+        </div>
     )
 }
 export default Instructor
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import StudentLoginForm from "../Componenets/StudentLoginForm.jsx";
 
 const Student = () => {
     return (
-        <div>Student</div>
+        <div className='student-login-container'>
+            <div className='student-login-content'>
+                <section>
+                    <div className='student-login-form'>
+                        <StudentLoginForm/>
+                    </div>
+                </section>
+            </div>
+        </div>
     )
 }
 export default Student
