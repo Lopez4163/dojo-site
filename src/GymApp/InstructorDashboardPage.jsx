8b96ca3473206aff03265c775e9c64c9ef92530a
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-03-29 15:36:54 -0500

    yerp

--- src/GymApp/InstructorDashboardPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const InstructorDashboardPage = () => {
-    return (
-        <div>InstructorDashboard</div>
-    )
-}
-export default InstructorDashboardPage
--- src/Pages/InstructorDashboardPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const InstructorDashboardPage = () => {
-    return (
-        <div>InstructorDashboardPage</div>
-    )
-}
-export default InstructorDashboardPage
